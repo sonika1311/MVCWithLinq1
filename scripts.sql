3ee55817042b0fa52452b39d46855dc8570d1b32
sonika1311 <sonikakumaribom@gmail.com>
2024-07-15 17:59:21 +0530

    scripts

--- scripts.sql
+++ scripts.sql
@@ -8,7 +8,9 @@ Status Bit
 Not Null Default 1)
 
 select * from Student
-
+delete from Student where Sid=105
 insert into Student values (1,'Tira',5,5009,null,1),
 (2,'Meet',7,4567,null,0),
 (3,'Sita',4,5678,null,1)
+
+update Student set Status=1 where Sid=105
